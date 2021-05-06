b339cfda8baea22502c03610c436f90ad77ecee1
RAHMANI Abd El Kader Seif El Islem <77025510+RAHAMNIabdelkaderseifelislem@users.noreply.github.com>
2021-05-06 12:29:48 +0100

    Initial commit

---
+++ DataBase/db_exam.sql
@@ -0,0 +1,181 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: May 06, 2021 at 01:26 PM
+-- Server version: 10.4.18-MariaDB
+-- PHP Version: 8.0.3
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `db_exam`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_admin`
+--
+
+CREATE TABLE `tbl_admin` (
+  `adminId` int(11) NOT NULL,
+  `adminUser` varchar(50) NOT NULL,
+  `adminPass` varchar(32) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `tbl_admin`
+--
+
+INSERT INTO `tbl_admin` (`adminId`, `adminUser`, `adminPass`) VALUES
+(1, 'admin', '202cb962ac59075b964b07152d234b70');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_ans`
+--
+
+CREATE TABLE `tbl_ans` (
+  `id` int(11) NOT NULL,
+  `quesNo` int(11) NOT NULL,
+  `rightAns` int(11) NOT NULL DEFAULT 0,
+  `ans` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `tbl_ans`
+--
+
+INSERT INTO `tbl_ans` (`id`, `quesNo`, `rightAns`, `ans`) VALUES
+(25, 1, 1, 'JAVA'),
+(26, 1, 0, 'SWIFT'),
+(27, 1, 0, 'C++'),
+(28, 1, 0, 'C#'),
+(29, 2, 0, 'MONGO DB'),
+(30, 2, 0, 'NEO4J'),
+(31, 2, 1, 'MySQL'),
+(32, 2, 0, 'noSQL'),
+(33, 3, 0, 'QT'),
+(34, 3, 1, 'Spring'),
+(35, 3, 0, 'Flask'),
+(36, 3, 0, 'XAMARIN'),
+(37, 4, 0, 'C'),
+(38, 4, 0, 'C++'),
+(39, 4, 0, 'JavaScript'),
+(40, 4, 1, 'Java');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_ques`
+--
+
+CREATE TABLE `tbl_ques` (
+  `id` int(11) NOT NULL,
+  `quesNo` int(11) NOT NULL,
+  `ques` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `tbl_ques`
+--
+
+INSERT INTO `tbl_ques` (`id`, `quesNo`, `ques`) VALUES
+(7, 1, 'Which one of this used by BIG DATA programmers?'),
+(8, 2, 'Which one of this used for relational data bases?'),
+(9, 3, 'Which one of this is framework for java?'),
+(10, 4, 'Which one of this is pure O.O.P programming language?');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_user`
+--
+
+CREATE TABLE `tbl_user` (
+  `userid` int(11) NOT NULL,
+  `name` varchar(50) NOT NULL,
+  `username` varchar(30) NOT NULL,
+  `password` varchar(32) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `status` int(11) NOT NULL DEFAULT 0
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `tbl_user`
+--
+
+INSERT INTO `tbl_user` (`userid`, `name`, `username`, `password`, `email`, `status`) VALUES
+(7, 'moh', 'moh213', 'e10adc3949ba59abbe56e057f20f883e', 'moh@moh.com', 0);
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `tbl_admin`
+--
+ALTER TABLE `tbl_admin`
+  ADD PRIMARY KEY (`adminId`);
+
+--
+-- Indexes for table `tbl_ans`
+--
+ALTER TABLE `tbl_ans`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `tbl_ques`
+--
+ALTER TABLE `tbl_ques`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `tbl_user`
+--
+ALTER TABLE `tbl_user`
+  ADD PRIMARY KEY (`userid`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `tbl_admin`
+--
+ALTER TABLE `tbl_admin`
+  MODIFY `adminId` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `tbl_ans`
+--
+ALTER TABLE `tbl_ans`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=41;
+
+--
+-- AUTO_INCREMENT for table `tbl_ques`
+--
+ALTER TABLE `tbl_ques`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+
+--
+-- AUTO_INCREMENT for table `tbl_user`
+--
+ALTER TABLE `tbl_user`
+  MODIFY `userid` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
